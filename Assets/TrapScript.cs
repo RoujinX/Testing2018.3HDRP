51a2c913b53e90310f5a83339aedde0bdfaa913c
RoujinX <roujin.x@gmail.com>
2019-04-09 14:16:51 +0200

    Spike Trap Script Modified Character for Collision Detection

---
+++ Assets/TrapScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapScript : MonoBehaviour
+{
+	public Animator controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		controller = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+	void OnTriggerEnter(Collider other)
+	{
+		controller.SetTrigger("TriggerTrap");
+		print("Triggered!");
+	}
+}
